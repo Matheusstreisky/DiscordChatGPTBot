d9a80775978646261b77d5246e289e910bc34057
Joao Pucci <joao.pucci@live.com>
2023-05-22 20:59:25 +0900

    Revert unwanted changes

--- src/main/java/com/streisky/discordchatgptbot/openai/OpenAIApi.java
+++ src/main/java/com/streisky/discordchatgptbot/openai/OpenAIApi.java
@@ -1,7 +1,7 @@
 package com.streisky.discordchatgptbot.openai;
 
+import com.theokanning.openai.OpenAiService;
 import com.theokanning.openai.completion.CompletionRequest;
-import com.theokanning.openai.service.OpenAiService;
 
 public class OpenAIApi {
 
